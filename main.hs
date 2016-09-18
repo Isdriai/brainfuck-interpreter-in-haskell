a439dbfe866cf99bf286b4e2c6ab1ea15e7eccd0
PicotRodolphe <picot.rodolphe@gmail.com>
2016-09-18 14:08:26 +0200

    je n'ai pas encore essaye de le compiler, mais je pense qu'il reste juste des erreurs de syntaxe

--- main.hs
+++ main.hs
@@ -1,44 +1,56 @@
+import System.Environment
+
 data Blist t = Blist { 
 	left :: [t],
-	elt :: t,
 	right :: [t] 
 } deriving (Show)
 
 deplacement_gauche :: Blist t -> Blist t
 deplacement_gauche blist=
-	Blist { left = tail (left blist), elt = bouge, right = bouge:(right blist) }
-	where bouge = head (left blist) 
+	case left blist of [] -> blist
+		    		   otherwise -> 
+	Blist { left = tail (left blist), right = head (left blist):(right blist) }
 
 deplacement_droite :: Blist t -> Blist t
 deplacement_droite blist=
-	Blist { left = bouge:(left blist), elt = bouge, right = tail (right blist) }
-	where bouge = head (right blist)	
+	Blist { left = head (right blist):(left blist), right = tail (right blist) }
 
-update :: Blist t -> Blist t
+update :: Blist t -> t -> Blist t
 update blist elem=
-	Blist { left = left blist, elt = elem, right = right blist}
+	Blist { left = left blist, right = elem:tail (right blist)}
 
 -- Renvoie un int qui identifiera si il y a une erreur ou pas
-acion :: int -> Blist Char -> Blist Int -> Int 
+acion :: int -> Blist Char -> Blist Int -> IO () 
 action saut instB mem =
 	if saut > 0 then 
-		case elt instB of ']' -> action (saut-1) (deplacement_droite instB) mem 
-						  '[' -> action (saut+1) (deplacement_droite instB) mem 
+		case right instB of ']':_ -> action (saut-1) (deplacement_droite instB) mem 
+						  '[':_ -> action (saut+1) (deplacement_droite instB) mem 
 					      otherwise -> action saut (deplacement_droite instB) mem
 	else if saut < 0 then 
-		case elt instB of ']' -> action (saut+1) (deplacement_gauche instB) mem
-						  '[' -> action (saut-1) (deplacement_gauche instB) mem
+		case right instB of ']':_ -> action (saut+1) (deplacement_gauche instB) mem
+						  '[':_ -> action (saut-1) (deplacement_gauche instB) mem
 			      		  otherwise -> action (deplacement_gauche instB) mem
 	else
-		case elt instB of '>' -> lecture (deplacement_droite mem)
-			              '<' -> lecture (deplacement_gauche mem)
-			              '+' -> lecture (update mem ((elt mem)+1))
-			              '-' -> lecture (update mem ((elt mem)-1))
-			              '.' -> 
-			              ',' -> 
-			              '[' -> action 1 (deplacement_droite instB) mem
-			              ']' -> action (-1) (deplacement_gauche instB) mem
-			              otherwise -> if saut != 0 then "error" else ()
+		case right instB of '>':_ -> lecture (deplacement_droite mem)
+			              '<':_ -> lecture (deplacement_gauche mem)
+			              '+':_ -> lecture (update mem ((elt mem)+1))
+			              '-':_ -> lecture (update mem ((elt mem)-1))
+			              '.':_ -> do putStrLn (show head (right mem))
+			              		   lecture mem
+			              ',':_ -> do arg <- getLine
+			              		   lecture (update mem (read arg :: Int))
+			              '[':_ -> if elt mem = 0 
+			              				then action 1 (deplacement_droite instB) mem 
+			              				else lecture mem
+			              ']':_ -> if elt mem =0 
+			              				then action (-1) (deplacement_gauche instB) mem 
+			              				else lecture mem
+			              [] -> "fin"
+			              otherwise -> lecture mem
 		where lecture = action saut (deplacement_droite instB)
 
-main = putStrLn "Hello World"
+main = do
+	args <- getArgs
+	action 0 instruction memoire
+	where instruction = Blist { left = [], right = concat args}
+		  memoire = Blist { left = repeat 0, elt = 0, right = repeat 0}
