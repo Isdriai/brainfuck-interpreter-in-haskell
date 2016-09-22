4383fae8cb2a15387ce016abbb907d26e6d6c093
PicotRodolphe <picot.rodolphe@gmail.com>
2016-09-22 16:30:26 +0200

    pb syntaxe where puis do

--- main.hs
+++ main.hs
@@ -5,52 +5,55 @@ data Blist t = Blist {
 	right :: [t] 
 } deriving (Show)
 
-deplacement_gauche :: Blist t -> Blist t
-deplacement_gauche blist=
-	case left blist of [] -> blist
-		    		   otherwise -> 
-	Blist { left = tail (left blist), right = head (left blist):(right blist) }
+--Il en va de l'utilisateur de mettre autant de '[' que de ']' ! Sinon il y aura une boucle infinie vu le fonctionnement de move_gauche
+move_gauche :: Blist t -> Blist t
+move_gauche blist=
+	case left blist of 
+		[]-> blist
+		_ -> Blist {left = tail (left blist), right = head (left blist):(right blist)}
 
-deplacement_droite :: Blist t -> Blist t
-deplacement_droite blist=
+move_droite :: Blist t -> Blist t
+move_droite blist=
 	Blist { left = head (right blist):(left blist), right = tail (right blist) }
 
 update :: Blist t -> t -> Blist t
 update blist elem=
 	Blist { left = left blist, right = elem:tail (right blist)}
 
-acion :: int -> Blist Char -> Blist Int -> IO () 
+action :: int -> Blist Char -> Blist Int -> IO () 
 action saut instB mem =
 	if saut > 0 then 
-		case right instB of ']':_ -> action (saut-1) (deplacement_droite instB) mem 
-						  '[':_ -> action (saut+1) (deplacement_droite instB) mem 
-					      otherwise -> action saut (deplacement_droite instB) mem
+		case right instB of 
+			']':_ -> action (saut-1) (move_droite instB) mem 
+			'[':_ -> action (saut+1) (move_droite instB) mem 
+			_ -> action saut (move_droite instB) mem
 	else if saut < 0 then 
-		case right instB of ']':_ -> action (saut+1) (deplacement_gauche instB) mem
-						  '[':_ -> action (saut-1) (deplacement_gauche instB) mem
-			      		  otherwise -> action (deplacement_gauche instB) mem
+		case right instB of 
+			']':_ -> action (saut+1) (move_gauche instB) mem
+			'[':_ -> action (saut-1) (move_gauche instB) mem
+			_ -> action (move_gauche instB) mem
 	else
-		case right instB of '>':_ -> lecture (deplacement_droite mem)
-			              '<':_ -> lecture (deplacement_gauche mem)
-			              '+':_ -> lecture (update mem ((valmem)+1))
-			              '-':_ -> lecture (update mem ((valmem)-1))
-			              '.':_ -> do putStrLn (show head (right mem))
-			              		   lecture mem
-			              ',':_ -> do arg <- getLine
-			              		   lecture (update mem (read arg :: Int))
-			              '[':_ -> if valmem mem = 0 
-			              				then action 1 (deplacement_droite instB) mem 
-			              				else lecture mem
-			              ']':_ -> if valmem mem =0 
-			              				then action (-1) (deplacement_gauche instB) mem 
-			              				else lecture mem
-			              [] -> putStrLn "fin programme"
-			              otherwise -> lecture mem
-		where lecture = action saut (deplacement_droite instB)
+		case right instB of 
+			'>':_ -> lecture (move_droite mem)
+			'<':_ -> lecture (move_gauche mem)
+			'+':_ -> lecture (update mem ((valmem)+1))
+			'-':_ -> lecture (update mem ((valmem)-1))
+			'.':_ -> do putStrLn (show valmem)
+					lecture mem
+			',':_ -> do arg <- getLine
+						lecture (update meme (read arg :: Int))
+			'[':_ -> if valmem == 0 
+							then action 1 (move_droite instB) mem 
+							else lecture mem
+			']':_ -> if valmem mem ==0 
+							then action (-1) (move_gauche instB) mem 
+							else lecture mem
+			[] -> putStrLn "fin programme"
+			_ -> lecture mem
+		where lecture = action saut (move_droite instB)
 			  valmem = head (right mem)
 
-main = do
-	args <- getArgs
-	action 0 instruction memoire
-	where instruction = Blist { left = [], right = concat args}
-		  memoire = Blist { left = repeat 0, right = repeat 0}
+main = do args <- getArgs
+		action 0 instruction 
+		where instruction = Blist { left = [], right = concat args}
+			  memoire = Blist { left = repeat 0, right = repeat 0}
